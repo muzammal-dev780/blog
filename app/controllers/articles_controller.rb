256375afe69bdf89f13d6fd16d93da84dfff2ef2
muzammal <your.email@example.com>
2020-07-28 08:51:26 +0500

    uptodate

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-	http_basic_authenticate_with name: "abc", password: "abc", except: [:index, :show]
+	#http_basic_authenticate_with name: "abc", password: "abc", except: [:index, :show]
 	def new
 		@article = Article.new
 	end
